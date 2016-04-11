a7858abc36e81d078e158f85b6264917d92f382b
macaba <macaba@gmail.com>
2016-04-11 18:42:03 +0100

    Added the ability to get the duration of the current state.
    This allows the user to determine how long the IMU has been magnetically
    jammed for, for warning/error purposes.

--- src/MagneticJammingFilter.h
+++ src/MagneticJammingFilter.h
@@ -21,6 +21,8 @@ private:
   float jammingThreshold = 5.0f;
   bool jammingActive = false;
   int jammingCounter = 0;
+  
+  elapsedMillis stateTime;
 
   float cal[16]; // 0-8=offsets, 9=field strength, 10-15=soft iron map
 //-------------------------------------------------------------------------------------------
@@ -46,8 +48,8 @@ public:
       //If a valid calibration exists, use it to tighten up the magnetic rejection
       memcpy(cal, buf+2, sizeof(cal));
       fieldIntensityFiltered = cal[9];
-      fieldIntensityFilteredMax = cal[9] + 8;
-      fieldIntensityFilteredMin = cal[9] - 8;
+      fieldIntensityFilteredMax = cal[9] + 10;
+      fieldIntensityFilteredMin = cal[9] - 10;
     }  
   }
   
@@ -55,7 +57,7 @@ public:
 		fieldIntensity = sqrtf(mx*mx + my*my + mz*mz);
 
 		if((fieldIntensity < (fieldIntensityFiltered + (jammingThreshold * 3))) && (fieldIntensity > (fieldIntensityFiltered - (jammingThreshold * 3)))) {
-			fieldIntensityFiltered += ((fieldIntensity - fieldIntensityFiltered) * 0.0001);
+			fieldIntensityFiltered += ((fieldIntensity - fieldIntensityFiltered) * 0.001);
 		}
 		if(fieldIntensityFiltered > fieldIntensityFilteredMax)
 			fieldIntensityFiltered = fieldIntensityFilteredMax;
@@ -70,28 +72,41 @@ public:
 			}
 		}
 		if((fieldIntensity > (fieldIntensityFiltered + jammingThreshold)) || (fieldIntensity < (fieldIntensityFiltered - jammingThreshold))) {
+			if(jammingActive == false)
+				stateTime = 0;	//Only reset elapsedMillis if jamming state is new
 			jammingCounter = 0;
-			jammingActive = true;
+			jammingActive = true;			
 			return;
 		}
 		else {
+			if(jammingActive == true)
+				stateTime = 0;	//Only reset elapsedMillis if jamming state is new
 			jammingCounter = 0;
 			jammingActive = false;
 		}
 	}
 
+  //Returns the latest calculated field intensity
   float getFieldIntensity() {
       return fieldIntensity;
   }
   
+  //Returns the value that is being used for the window centre
   float getFieldIntensityFiltered(){
     return fieldIntensityFiltered;
   }
   
+  //Returns the jamming status, i.e. true = jamming is active, magnetometer readings are not being used in AHRS calculation.
   bool getJammingStatus(){
     return jammingActive;
-	} 
+  } 
   
+  //Returns the duration, in milliseconds, of the current jamming state
+  long getTime(){
+	return stateTime;
+  }
+  
+  //Sets the allowable window for field intensity. i.e. window size of 10 means an allowable range of fieldIntensityFiltered +/- 5
   void setFieldIntensityWindow(float fieldIntensityWindow){
     if(fieldIntensityWindow < 1.0f)
       return;
